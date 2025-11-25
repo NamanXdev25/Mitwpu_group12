0dcf80fc63c0d635c2447ad306eefa62f3f62552
sagusokay <shloka.shetty@mitwpu.edu.in>
2025-11-25 16:56:10 +0530

    xib label

---
+++ BreastCancerApp/View/MedicationItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  MedicationItemCell.swift
+//  BreastCancerApp
+//
+//  Created by SDC-USER on 25/11/25.
+//
+
+import UIKit
+
+class MedicationItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
