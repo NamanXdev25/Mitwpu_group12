508e679df90278405c3af435f2b8dcedda70e579
NamanBhansali <namanbhansali006@gmail.com>
2025-12-15 16:19:02 +0530

    HomeScreen done

---
+++ BreastCancerApp/HomeArticleCell.swift
@@ -0,0 +1,30 @@
+//
+//  HomeArticleCell.swift
+//  BreastCancerApp
+//
+//  Created by Naman Bhansali on 15/12/25.
+//
+
+import UIKit
+
+class HomeArticleCell: UICollectionViewCell {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var articleImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        containerView.layer.cornerRadius = 16
+        containerView.clipsToBounds = true
+        articleImageView.contentMode = .scaleAspectFill
+        articleImageView.clipsToBounds = true
+    }
+    
+    func configure(with model: HomeArticleModel) {
+        titleLabel.text = model.title
+        subtitleLabel.text = model.subtitle
+        articleImageView.image = UIImage(named: model.imageName)
+    }
+}
---
+++ BreastCancerApp/HomeMemoryCell.swift
@@ -0,0 +1,36 @@
+//
+//  HomeMemoryCell.swift
+//  BreastCancerApp
+//
+//  Created by Naman Bhansali on 15/12/25.
+//
+
+import UIKit
+
+class HomeMemoryCell: UICollectionViewCell {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var memoryImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Style the image view
+        memoryImageView.layer.cornerRadius = 12
+        memoryImageView.clipsToBounds = true
+        memoryImageView.contentMode = .scaleAspectFill
+        
+        // Optional: Style container if needed
+        // containerView.layer.cornerRadius = 12
+    }
+    
+    func configure(with model: HomeMemoryModel) {
+        memoryImageView.image = UIImage(named: model.imageName)
+        
+        // Mapping model data to the new labels
+        titleLabel.text = model.date
+        subtitleLabel.text = model.description
+    }
+}
