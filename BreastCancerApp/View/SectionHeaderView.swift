b22dea586becbe4f0597f9f8d9ed4807bbc90be7
Shivani Dinesh <shivani.dinesh13@gmail.com>
2026-01-06 10:54:20 +0530

    1

---
+++ BreastCancerApp/View/SectionHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SectionHeaderView.swift
+//  BreastCancerApp
+//
+//  Created by Shivani Dinesh on 06/01/26.
+//
+
+import UIKit
+
+class SectionHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
