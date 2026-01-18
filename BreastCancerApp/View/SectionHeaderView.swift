21fc06ea345edf185af4306f0e70cd868450e1e3
NamanBhansali <namanbhansali006@gmail.com>
2026-01-18 16:00:08 +0530

    Cleaned branch after merge

--- BreastCancerApp/View/SectionHeaderView.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class SectionHeaderView: UICollectionReusableView {
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-}
