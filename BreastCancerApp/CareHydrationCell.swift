572a782902937e916fd89c9428479e666bf0ccd5
NamanBhansali <namanbhansali006@gmail.com>
2026-02-05 01:39:02 +0530

    Constraints Updated

--- BreastCancerApp/CareHydrationCell.swift
+++ BreastCancerApp/CareHydrationCell.swift
@@ -193,11 +193,11 @@ class CareHydrationCell: UICollectionViewCell {
         detailContainerView.layer.cornerRadius = 0
         
         // Subtle shadow on the cell itself
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 2)
-        self.layer.shadowRadius = 4
-        self.layer.shadowOpacity = 0.05
-        self.layer.masksToBounds = false
+//        self.layer.shadowColor = UIColor.black.cgColor
+//        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+//        self.layer.shadowRadius = 4
+//        self.layer.shadowOpacity = 0.05
+//        self.layer.masksToBounds = false
         
         self.contentView.clipsToBounds = true
         
