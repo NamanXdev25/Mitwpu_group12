392632bea678ce45244c3cb6482d5de09b134e91
NamanBhansali <namanbhansali006@gmail.com>
2026-02-25 22:49:36 +0530

    Code refined

--- BreastCancerApp/CareHeaderCell.swift
+++ BreastCancerApp/CareHeaderCell.swift
@@ -1,25 +1,3 @@
-//import UIKit
-//
-//class CareHeaderCell: UICollectionViewCell {
-//
-//    @IBOutlet weak var Titlelabel: UILabel!
-//    @IBOutlet weak var Managelabel: UILabel!
-//    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//
-//        Titlelabel.text = ""
-//        Managelabel.text = "Manage"
-//    }
-//
-//    func configure(title: String, showManage: Bool) {
-//        Titlelabel.text = title
-//        Managelabel.isHidden = !showManage
-//        
-//    }
-//}
-
-
 import UIKit
 
 // MARK: - Delegate Protocol
--- BreastCancerApp/CareMedicationCell.swift
+++ BreastCancerApp/CareMedicationCell.swift
@@ -1,26 +1,3 @@
-//import UIKit
-//
-//class CareMedicationCell: UICollectionViewCell {
-//
-//    @IBOutlet weak var MedicationConatiner: UIView!
-//    @IBOutlet weak var MedicationImage: UIImageView!
-//    @IBOutlet weak var MedicationLabel: UILabel!
-//    @IBOutlet weak var TakenLabel: UILabel!
-//    @IBOutlet weak var MedicationInfoButton: UIButton!
-//    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Styling the container - we'll keep it simple as you'll set radius in Storyboard
-//        self.contentView.layer.masksToBounds = true
-//    }
-//
-//    func configure(title: String, status: String, image: UIImage?) {
-//        MedicationLabel.text = title
-//        TakenLabel.text = status
-//        MedicationImage.image = image
-//    }
-//}
-
 import UIKit
 
 // MARK: - Delegate Protocol
