be1818ed3a79e1b14d1afc70689cf6f466b495f9
gayatri <gayatrigoundadkar@gmail.com>
2026-03-22 22:30:13 +0530

    home screen pop up added

---
+++ BreastCancerApp/View/MemorySavedPopupView.swift
@@ -0,0 +1,17 @@
+//
+//  MemorySavedPopupView.swift
+//  BreastCancerApp
+//
+//  Created by Gayatri Goundadkar on 22/03/26.
+//
+
+import UIKit
+
+class MemorySavedPopupView: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
