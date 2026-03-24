445b95107f1b32190eff44e0ba7305f86218ad5f
Shivani Dinesh <shivani.dinesh13@gmail.com>
2026-03-24 12:29:02 +0530

    exercise videos

--- BreastCancerApp/NewExerciseFolder/ExercisePlayerViewController.swift
+++ BreastCancerApp/NewExerciseFolder/ExercisePlayerViewController.swift
@@ -103,7 +103,7 @@ class ExercisePlayerViewController: UIViewController {
     }
 
     private func infoSection() -> NSCollectionLayoutSection {
-        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(132))
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(146))
         return NSCollectionLayoutSection(group: .vertical(layoutSize: size, subitems: [.init(layoutSize: size)]))
     }
 
